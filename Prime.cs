2ee450486b43ab02508dbf3cfd7255abfa5919c4
Hush <36056305+Jollof-guy@users.noreply.github.com>
2020-06-18 09:22:30 +0100

    Sharp First
    Print Prime First

---
+++ Prime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWorld
+{
+    class Prime
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Program prints Prime numbers");
+            bool notPrime = false;  
+            int j;  
+  
+            Console.Write("What is your limit:");  
+            int limit = Int32.Parse(Console.ReadLine());  
+  
+            for (int i = 2; i <= limit; i++)  
+            {  
+                for (j = 2; j < i; j++)  
+                {  
+                    if (i % j == 0)  
+                    {  
+                        notPrime = true;  
+                        break;  
+                    }  
+                }  
+                  
+                if (!notPrime)  
+                    Console.WriteLine("{0}", j);  
+                else  
+                    notPrime = false;  
+            }
+
+            Console.ReadLine();
+        }  
+    }
+}
